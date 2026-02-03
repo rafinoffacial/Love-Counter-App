1a8b3397077212a9e1eeb9b7947ecdd03f47fd94
rafinoffacial <146970160+rafinoffacial@users.noreply.github.com>
2026-02-03 22:47:58 +0000

    Fix: Switched to JSON config to remove TypeScript requirement

--- capacitor.config.ts
+++
@@ -1,8 +0,0 @@
-import { CapacitorConfig } from '@capacitor/cli';
-const config: CapacitorConfig = {
-  appId: 'com.nawal.lovecounter',
-  appName: 'Love Counter',
-  webDir: 'www',
-  bundledWebRuntime: false
-};
-export default config;
